dc6fd84dbc50acad1f2532723a0632f7fa0c7b5a
AbigaelP <150002397+AbigaelP@users.noreply.github.com>
2024-03-26 11:36:32 +0100

    MAJ script pipe

--- Assets/Scripts/Pipes.cs
+++ Assets/Scripts/Pipes.cs
@@ -2,20 +2,21 @@ using UnityEngine;
 
 public class Pipes : MonoBehaviour
 {
-    public float speed = 5f;
+    private readonly float speed = 5f; // la vitesse de déplacement des pipes
 
-    private float leftEdge;
+
+    private float leftEdge; // la gauche de l'écran
 
     private void Start()
     {
-        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x -1f;
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 5f; // calcul la position la plus à gauche de l'écran
     }
 
     private void Update()
     {
-        transform.position += Vector3.left * speed * Time.deltaTime;
-        
-        if (transform.position.x < leftEdge)
+        transform.position += speed * Time.deltaTime * Vector3.left; // déplacer la pièce pour donner l'impression d'avancer
+
+        if (transform.position.x < leftEdge) // on cherche à détruire la pièce une fois qu'elle disparaît de l'écran
         {
             Destroy(gameObject);
         }
